5bde4dc29d591c1ffaca9eff8db2e0aefdd8f87f
Enijar <TheEnijar@gmail.com>
2019-08-17 17:42:13 +0100

    Calculate popup location when hot spot is clicked on map

--- src/app/core/utils.js
+++ src/app/core/utils.js
@@ -5,3 +5,14 @@ export const url = pathname => {
 export const asset = pathname => {
   return url(pathname);
 };
+
+export const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+
+export const getOffset = element => {
+  const bound = element.getBoundingClientRect();
+  const html = document.documentElement;
+  return {
+    top: bound.top + window.pageYOffset - html.clientTop,
+    left: bound.left + window.pageXOffset - html.clientLeft
+  };
+};
