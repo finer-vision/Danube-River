76fa5ba76ae87132c090a7b1823fb8fd5fcd31cb
James Craig <jamescraig@finervision.com>
2019-09-10 17:57:09 +0100

    Fixed assets script

--- bin/generateAssetsPreload.js
+++ bin/generateAssetsPreload.js
@@ -1,33 +1,16 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-
-const allowedExtensions = ['png', 'jpg', 'jpeg', 'gif', 'svg'];
-
-function getFiles(directory, allFiles = []) {
-  const files = fs.readdirSync(directory, {withFileTypes: true});
-
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    const filePath = path.join(directory, file.name);
-    const fileParts = filePath.split('.');
-    const fileExtension = fileParts[fileParts.length - 1];
-
-    if (file.isDirectory()) {
-      allFiles.push(...getFiles(filePath, allFiles));
-      continue;
-    }
-
-    if (file.name.startsWith('.') || !allowedExtensions.includes(fileExtension)) {
-      continue;
-    }
-
-    allFiles.push(filePath);
-  }
-
-  return allFiles;
-}
+const recursive = require('recursive-readdir');
 
 const srcDirectory = path.resolve(__dirname, '..', 'src');
-const files = [...new Set(getFiles(path.join(srcDirectory, 'assets')).map(file => file.replace(srcDirectory, '')))];
-fs.writeFileSync(path.resolve(__dirname, '..', 'src', 'assets.json'), JSON.stringify(files, null, 2));
+const allowedExtensions = ['png', 'jpg', 'jpeg', 'gif', 'svg'];
+recursive(path.join(srcDirectory, 'assets'), allowedExtensions, (err, files) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  for (let i = 0; i < files.length; i++) {
+    files[i] = files[i].replace(srcDirectory, '');
+  }
+  fs.writeFileSync(path.join(srcDirectory, 'assets.json'), JSON.stringify(files, null, 2));
+});
