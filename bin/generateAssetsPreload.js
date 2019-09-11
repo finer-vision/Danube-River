d49e21b9e71a157e64d4fba9b93a1fcee1a7c244
James Craig <jamescraig@finervision.com>
2019-09-11 13:07:54 +0100

    Fixed generate assets preload script and regenerated assets.json

--- bin/generateAssetsPreload.js
+++ bin/generateAssetsPreload.js
@@ -9,8 +9,25 @@ recursive(path.join(srcDirectory, 'assets'), allowedExtensions, (err, files) => 
   if (err) {
     return console.error(err.message);
   }
+
+  const assets = [];
   for (let i = 0; i < files.length; i++) {
     files[i] = files[i].replace(srcDirectory, '');
+
+    // Check if extension is allowed
+    let allowed = false;
+    for (let j = 0; j < allowedExtensions.length; j++) {
+      if (files[i].endsWith(allowedExtensions[j])) {
+        allowed = true;
+        break;
+      }
+    }
+
+    if (!allowed) {
+      continue;
+    }
+
+    assets.push(files[i]);
   }
-  fs.writeFileSync(path.join(srcDirectory, 'assets.json'), JSON.stringify(files, null, 2));
+  fs.writeFileSync(path.join(srcDirectory, 'assets.json'), JSON.stringify(assets, null, 2));
 });
