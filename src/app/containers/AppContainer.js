791ec4c6cb96cb4a0d2a99bd994dd98cc9dcf760
James Craig <jamescraig@finervision.com>
2019-09-13 15:05:05 +0100

    Removed debug code

--- src/app/containers/AppContainer.js
+++ src/app/containers/AppContainer.js
@@ -79,7 +79,7 @@ export default class AppContainer extends Component {
 
   async componentDidMount() {
     const isMobile = (new MobileDetect(window.navigator.userAgent)).mobile() !== null;
-    // !isMobile && await preloadAssets(progress => this.setState({loadingProgress: Math.ceil(progress)}));
+    !isMobile && await preloadAssets(progress => this.setState({loadingProgress: Math.ceil(progress)}));
     const mapComponent = await import(`../components/${isMobile ? 'MobileMap' : 'Map/index'}`);
     this.setState({loading: false, isMobile, mapComponent: mapComponent.default});
     window.addEventListener('resize', this.#handleResize);
