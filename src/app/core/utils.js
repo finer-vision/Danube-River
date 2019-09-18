6fa5927338be2dbf39b6bb1278f8111f5fe3e1dd
James Craig <jamescraig@finervision.com>
2019-09-18 09:08:34 +0100

    Relative links to images

--- src/app/core/utils.js
+++ src/app/core/utils.js
@@ -1,6 +1,8 @@
 import assets from "../../assets.json";
 
-export const asset = pathname => pathname;
+export const asset = pathname => {
+  return pathname.replace(/^\//, '');
+};
 
 export const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
 
