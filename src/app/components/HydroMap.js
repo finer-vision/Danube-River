375fe66468f6002b149c63128f5c7d5b579cb9c8
Tadas <tadas@brand42.co.uk>
2019-09-20 12:01:06 +0100

    animate the dots when map becomes visible on scroll

--- src/app/components/HydroMap.js
+++ src/app/components/HydroMap.js
@@ -1,14 +1,23 @@
 import React, {Component} from "react";
 import {asset} from "../core/utils";
 import LazyImage from "./LazyImage";
+import {Waypoint} from "react-waypoint";
 
 export default class HydroMap extends Component {
+  state = {
+    focused: false
+  }
+
+  #toggleFocused = focused => () => this.setState({focused});
+
   render() {
     return (
-      <div className="HydroMap">
+      <div className={`HydroMap ${this.state.focused ? 'HydroMap--focused' : ''}`}>
         <LazyImage src={asset('assets/img/hydromap/medium.svg')} className="HydroMap__layer"/>
         <LazyImage src={asset('assets/img/hydromap/large.svg')} className="HydroMap__layer"/>
         <LazyImage src={asset('assets/img/hydromap/map.jpg')}/>
+
+        <Waypoint onEnter={this.#toggleFocused(true)} onLeave={this.#toggleFocused(false)}/>
       </div>
     );
   }
