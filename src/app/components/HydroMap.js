e0796563717792b34e8a9fc02a767415a730b5df
Tadas <tadas@brand42.co.uk>
2019-09-20 12:26:59 +0100

    updated map, added stats text

--- src/app/components/HydroMap.js
+++ src/app/components/HydroMap.js
@@ -15,7 +15,14 @@ export default class HydroMap extends Component {
       <div className={`HydroMap ${this.state.focused ? 'HydroMap--focused' : ''}`}>
         <LazyImage src={asset('assets/img/hydromap/medium.svg')} className="HydroMap__layer"/>
         <LazyImage src={asset('assets/img/hydromap/large.svg')} className="HydroMap__layer"/>
-        <LazyImage src={asset('assets/img/hydromap/map.jpg')}/>
+        <LazyImage src={asset('assets/img/hydromap/hydromap.jpg')}/>
+
+        <div className="Stats Stats--hydromap">
+          <div className="Stats__number">
+            <span className="Stats--big">62</span> <span className="Stats--small"> dams</span>
+          </div>
+          <h4 class="type-h4">on the Danube - 59 of which are on its first 1,000km</h4>
+        </div>
 
         <Waypoint onEnter={this.#toggleFocused(true)} onLeave={this.#toggleFocused(false)}/>
       </div>
